f85e938f80e5240e109668205e53d14dd700c440
Filippo Saracco <filippo.saracco@stud.itsaltoadriatico.it>
2022-10-27 12:21:03 +0200

    Aggiunta db postgres

---
+++ server/connection.js
@@ -0,0 +1,19 @@
+var Client = require('pg')
+
+function GetDbClient(){
+
+}
+
+function GetDrones(){
+
+}
+
+function GetDrone(Id) {
+
+}
+
+function InsertValue(Value) {
+
+}
+
+module.exports()
--- server/index.js
+++ server/index.js
@@ -1,7 +1,21 @@
 var restify = require('restify');
+var { Client } = require('pg');
 
 var server = restify.createServer();
 server.use(restify.plugins.bodyParser());
+
+//connessione al DB
+async function getDbClient() {
+    var client = new Client({
+        user: 'drone',
+        host: '127.0.0.1',
+        database: 'drone',
+        password: 'Vmware1!',
+        port: 5432,
+    });
+    client.connect();
+    return client; //Apre la connessione
+}
 
 server.get('/drones', function(req, res, next) {
     res.send('List of drones: [TODO]');
@@ -16,6 +30,9 @@ server.get('/drones/:id', function(req, res, next) {
 server.post('/drones/:id', function(req, res, next) {
     res.send('Data received from drone [TODO]');
 
+    var id = req.params.id;
+    console.log(id);
+    console.log(JSON.parse(req.body));
     console.log(req.body);
 
     return next();
